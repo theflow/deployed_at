b2fc4c774488a605682ec82c4d1efe4f573645eb
Florian Munz <surf@theflow.de>
2009-09-24 00:10:31 +0200

    use factory methods also in the model tests

--- test/test_models.rb
+++ test/test_models.rb
@@ -1,6 +1,22 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 class DeployTest < Test::Unit::TestCase
+  def setup
+    repository(:default) do
+      transaction = DataMapper::Transaction.new(repository)
+      transaction.begin
+      repository.adapter.push_transaction(transaction)
+    end
+  end
+
+  def teardown
+    repository(:default) do
+      while repository.adapter.current_transaction
+        repository.adapter.current_transaction.rollback
+        repository.adapter.pop_transaction
+      end
+    end
+  end
 
   test 'number of changeset should be zero if zero changesets exist' do
     deploy = Deploy.new(:scm_log => '')
@@ -21,9 +37,9 @@ class DeployTest < Test::Unit::TestCase
   end
 
   test 'should create multiple subscriptions' do
-    user = User.create
-    project1 = Project.create(:name => 'project_1')
-    project2 = Project.create(:name => 'project_2')
+    user = create_user
+    project1 = create_project(:name => 'project_1')
+    project2 = create_project(:name => 'project_2')
 
     user.manage_subscriptions({ project1.id.to_s => 'on', project2.id.to_s => 'on' })
 
@@ -33,10 +49,10 @@ class DeployTest < Test::Unit::TestCase
   end
 
   test 'should not create multiple subscriptions' do
-    user = User.create
-    project1 = Project.create(:name => 'project_1')
-    project2 = Project.create(:name => 'project_2')
-    Subscription.create(:project => project1, :user => user)
+    user = create_user
+    project1 = create_project(:name => 'project_1')
+    project2 = create_project(:name => 'project_2')
+    create_subscription(:project => project1, :user => user)
 
     user.manage_subscriptions({ project1.id.to_s => 'on', project2.id.to_s => 'on' })
 
@@ -47,10 +63,10 @@ class DeployTest < Test::Unit::TestCase
   end
 
   test 'should create and destroy subscriptions' do
-    user = User.create
-    project1 = Project.create(:name => 'project_1')
-    project2 = Project.create(:name => 'project_2')
-    Subscription.create(:project => project1, :user => user)
+    user = create_user
+    project1 = create_project(:name => 'project_1')
+    project2 = create_project(:name => 'project_2')
+    create_subscription(:project => project1, :user => user)
 
     user.manage_subscriptions({ project2.id.to_s => 'on' })
 
@@ -60,17 +76,17 @@ class DeployTest < Test::Unit::TestCase
   end
 
   test 'should not email anything when there are no subscriptions' do
-    project = Project.create(:name => 'project_1')
+    project = create_project
 
     DeployMailer.expects(:send_by_smtp).never
     project.deploys.create(:title => 'title', :user => 'user', :scm_log => 'log', :head_rev => '42', :current_rev => '23')
   end
 
   test 'should notify subscribers for a new deploy' do
-    project1 = Project.create(:name => 'project_1')
-    project2 = Project.create(:name => 'project_2')
-    Subscription.create(:project => project1, :user => User.create(:email => 'user1@test.com'))
-    Subscription.create(:project => project2, :user => User.create(:email => 'user2@test.com'))
+    project1 = create_project(:name => 'project_1')
+    project2 = create_project(:name => 'project_2')
+    create_subscription(:project => project1, :user => create_user(:email => 'user1@test.com'))
+    create_subscription(:project => project2, :user => create_user(:email => 'user2@test.com'))
 
     DeployMailer.expects(:send_by_smtp).with(anything, anything, ['user1@test.com']).once
     project1.deploys.create(:title => 'title', :user => 'user', :scm_log => 'log', :head_rev => '42', :current_rev => '23')
